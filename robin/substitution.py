63be0565fb5fef32404b9087507fd3f609185064
Robin Newton <rnewton@fastmail.fm>
2016-06-20 22:36:20 +0100

    Extended substitution solver for polysubstitution

--- robin/substitution.py
+++ robin/substitution.py
@@ -11,81 +11,79 @@ ALPHABET = list(string.uppercase)
 
 ENGLISH_FREQUENCY = sorted(ALPHABET, key=lambda ch: -UNIGRAMS.freq(ch))
 
-DEBUG = False
 
+class SubstitutionSolver(object):
+    def __init__(self, ngram_set=QUADGRAMS):
+        self.ngram_set = ngram_set
+        self.debug = False
 
-def decrypt(key, text):
-    table = dict(zip(key, ALPHABET))
-    return ''.join(table.get(ch, ch) for ch in text.upper())
+    # Start with a reasonable guess as to the key (assume order of most
+    # frequent letters in the plain text is the same as in the training
+    # corpus) and try out small random changes to see if they improve the
+    # result until we can't seem to improve it any more. See
+    # http://practicalcryptography.com/cryptanalysis/stochastic-searching/cryptanalysis-simple-substitution-cipher/
+    def solve(self, text):
+        ngram_set = self.ngram_set
+        ciphertext = self.get_ciphertext(text)
 
+        def get_improvement(key):
+            score = ngram_set.score(self.decrypt(key, ciphertext))
+            if self.debug: print score
+            for _ in range(1000):
+                new_key = self.change_key(key)
+                new_score = ngram_set.score(self.decrypt(new_key, ciphertext))
+                if new_score > score:
+                    return new_key
+            return None
 
-def make_random_key():
-    key = list(ALPHABET)
-    random.shuffle(key)
-    return key
+        last_key = self.make_reasonable_key(ciphertext)
+        key = last_key
+        while key is not None:
+            last_key, key = key, get_improvement(key)
+        return last_key, self.decrypt(last_key, ciphertext)
 
+    def make_reasonable_key(self, ciphertext):
+        freqs = {}
+        for ch in self.get_cipher_alphabet(ciphertext):
+            freqs[ch] = 0
 
-def change_key(old_key):
-    key = list(old_key)
-    i, j = tuple(random.sample(range(len(key)), 2))
-    key[i], key[j] = key[j], key[i]
-    return key
+        for ch in ciphertext:
+            if ch in freqs:
+                freqs[ch] += 1
 
+        l = [(count, ch) for (ch, count) in freqs.items()]
+        l.sort()
+        l.reverse()
+        ordered_by_frequency = [ch for (_, ch) in l]
 
-def make_reasonable_key(text):
-    rev_table = dict(zip(ENGLISH_FREQUENCY, order_by_frequency(text)))
-    return map(rev_table.get, ALPHABET)
+        rev_table = dict(zip(ENGLISH_FREQUENCY, ordered_by_frequency))
+        return map(rev_table.get, ALPHABET)
 
+    def decrypt(self, key, ciphertext):
+        table = dict(zip(key, ALPHABET))
+        return ''.join(table.get(ch, ch) for ch in ciphertext)
 
-def order_by_frequency(chars):
-    freqs = {}
-    for ch in ALPHABET:
-        freqs[ch] = 0
+    def change_key(self, old_key):
+        key = list(old_key)
+        i, j = tuple(random.sample(range(len(key)), 2))
+        key[i], key[j] = key[j], key[i]
+        return key
 
-    for ch in chars:
-        ch = ch.upper()
-        if ch in freqs:
-            freqs[ch] += 1
+    def get_ciphertext(self, text):
+        return text.upper()
 
-    l = [(count, ch) for (ch, count) in freqs.items()]
-    l.sort()
-    l.reverse()
-    return [ch for (_, ch) in l]
-
-
-# Start with a reasonable guess as to the key (assume order of most frequent
-# letters in the plain text is the same as in the training corpus) and try out
-# small random changes to see if they improve the result until we can't seem
-# to improve it any more. See
-# http://practicalcryptography.com/cryptanalysis/stochastic-searching/cryptanalysis-simple-substitution-cipher/
-def solve(text, ngram_set=None):
-    if ngram_set is None:
-        ngram_set = QUADGRAMS
-
-    def get_improvement(key):
-        score = ngram_set.score(decrypt(key, text))
-        if DEBUG: print score
-        for _ in range(1000):
-            new_key = change_key(key)
-            new_score = ngram_set.score(decrypt(new_key, text))
-            if new_score > score:
-                return new_key
-        return None
-
-    last_key =  make_reasonable_key(text)
-    key = last_key
-    while key is not None:
-        last_key, key = key, get_improvement(key)
-    return last_key
+    def get_cipher_alphabet(self, ciphertext):
+        return ALPHABET
 
 
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 2:
+        solver = SubstitutionSolver()
         with open(sys.argv[1]) as f:
             text = f.read()
-        key = solve(text)
+        key, plaintext = solver.solve(text)
         print key
-        print decrypt(key, text)
+        print plaintext
     else:
         print >>sys.stderr, "Usage %s filename" % sys.argv[0]
